e5958eb6fcf212650dd112b7eb8a224ee2dcd1b6
kaunder <kaunder@ucalgary.ca>
2016-04-12 22:26:23 -0600

    add fxn to get random input from /dev/urandom

--- usebefore.c
+++ usebefore.c
@@ -10,13 +10,17 @@
 #include<string.h>
 #include<ctype.h>
 
+#define NUM_BYTES 64
+#define debug_flag 0
+
 //read in something...from a network?
 //then use it shittily all over the place
 
 
 //Function prototypes
 void usage();
-void getInput();
+void debug(char *str);
+void getInput(char *buf);
 void foo();
 void bar();
 void baz();
@@ -29,20 +33,48 @@ int main(int argc, char *argv[]){
     return 0;
     }*/
 
-  printf("%s\n", "What what");
+
+  //Define our input buffer here
+  char *randobuf = malloc(NUM_BYTES+1);
+
+  //Get a bunch o random input
+  getInput(randobuf);
+
+  printf("%s", randobuf);
+  
 
   return 0;
 }
 
 //Helper function to print a usage message
 void usage(){
-  printf("%s\n","Put your usage message here");
+  printf("%s\n","Usage: ./useb4 | hexdump");
 }
 
-void getInput(){
+void getInput(char *buf){
 
-  //read a bunch of bytes from /dev/random
+  debug("We're gonna get input now...");
+  
+  //read a bunch of bytes from /dev/urandom
+  //Be aware - sometimes you'll get zeros off of /dev/urandom,
+  //and so if you're piping into hexdump it'll look like you
+  //have less output. But it's just bc hexdump doesn't print the 0s.
+  //(don't panic)
+  FILE *f;
+  f=fopen("/dev/urandom", "r");
+  fread(buf, 1, NUM_BYTES, f);
+  fclose(f);
+
+  //null terminate that shit
+  buf[NUM_BYTES]='\0';
+
   
   
-  
+  debug("Done getting input from /dev/random.");
+}
+
+void debug(char *str){
+  if(debug_flag){
+    printf("%s\n",str);
+  }
 }

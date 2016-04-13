e49c861aeb65d9d3bef86fd6f036d06825a6fdab
kaunder <kaunder@ucalgary.ca>
2016-04-12 21:17:13 -0600

    initial commit

---
+++ usebefore.c
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////
+// Third shotgun parser property                             //
+// (use before full recognition)                             //
+// Katie Underwood                                           //
+//                                                           //
+///////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+
+//read in something...from a network?
+//then use it shittily all over the place
+
+
+//Function prototypes
+void usage();
+void getInput();
+void foo();
+void bar();
+void baz();
+
+int main(int argc, char *argv[]){
+
+  //If required, check here for required number/type of arguments
+  /*if(argc==1){//or whatever it should be
+    usage();
+    return 0;
+    }*/
+
+  printf("%s\n", "What what");
+
+  return 0;
+}
+
+//Helper function to print a usage message
+void usage(){
+  printf("%s\n","Put your usage message here");
+}
+
+void getInput(){
+
+  //read a bunch of bytes from /dev/random
+  
+  
+  
+}
